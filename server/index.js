224cc9e40ba8a94352e721c98d3d9caeaff0e547
Thananon Chounudom <thananonchounudom@Thananons-MacBook-Pro.local>
2026-01-19 15:09:06 +0700

    Fix: runtime crash by resolving Express 5 wildcard route issue

--- server/index.js
+++ server/index.js
@@ -756,7 +756,7 @@ app.put('/api/reservations/:id/cancel', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
